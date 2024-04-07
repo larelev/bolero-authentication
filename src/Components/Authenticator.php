cfed88f4f37a19a5a38cbbd19a57c678be48f686
David Blanchard <dpjb@hotmail.fr>
2024-04-07 11:41:13 +0200

    Refactor a bit.
    Can now list plugins commands on the console.
    
    TODO: use plugins commands namespace in front of the command name.

--- src/Components/Authenticator.php
+++ src/Components/Authenticator.php
@@ -3,7 +3,7 @@
 namespace Bolero\Plugins\Authentication\Components;
 
 use Bolero\Framework\Session\SessionInterface;
-use Bolero\Plugins\Authentication\Authentication;
+use Bolero\Plugins\Authentication\Configuration;
 use Bolero\Plugins\Authentication\Repositories\AuthenticationRepositoryInterface;
 
 class Authenticator implements AuthenticatorInterface
@@ -41,23 +41,23 @@ class Authenticator implements AuthenticatorInterface
     public function login(AuthenticationInterface $user): void
     {
         $this->session->start();
-        $this->session->write(Authentication::AUTH_KEY, $user->getAuthId());
+        $this->session->write(Configuration::AUTH_KEY, $user->getAuthId());
         $this->user = $user;
     }
 
     public function logout(): void
     {
-        $this->session->remove(Authentication::AUTH_KEY);
+        $this->session->remove(Configuration::AUTH_KEY);
     }
 
     public function isAuthenticated(): bool
     {
-        return $this->session->has(Authentication::AUTH_KEY);
+        return $this->session->has(Configuration::AUTH_KEY);
     }
 
     public static function hasLoggedIn(SessionInterface $session): bool
     {
-        $auth = $session->has(Authentication::AUTH_KEY);
+        $auth = $session->has(Configuration::AUTH_KEY);
         return $auth;
     }
 
